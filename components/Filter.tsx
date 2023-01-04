25d3f326a35b06fe824387f817fb0fa692fb1d1e
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-01-04 19:42:45 +0100

    need to fix pagination

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -44,7 +44,7 @@ const Filter = () => {
             <option value="highToLow">High to Low</option>
           </select>
           <select onChange={(e) => updatePageSize(e.target.value)} className="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm">
-            <option value="">Page Size</option>
+            <option value="5">Page Size</option>
             <option value="5">5</option>
             <option value="10">10</option>
             <option value="15">15</option>
